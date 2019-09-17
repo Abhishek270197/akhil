9712e2a7f76bfa92b23073c56152ef00954191a4
Abhishek270197 <abhishek_capgemini@outlook.com>
2019-09-17 10:52:23 +0530

    ak

---
+++ GreatOutdoorsAkhil/GreatOutdoors.Entities/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatOutdoors.Entities
+{
+    class Cart
+    {
+        public int CartId { get; set; }
+        public int AddressID { get; set; }
+        public int RetailerID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public int PriceAtOrder { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
---
+++ GreatOutdoorsAkhil/GreatOutdoors.Entities/GreatOutdoorsEntities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatOutdoors.Entities
+{
+    public class Order
+    {
+        public static int OrderId = 0;
+        public DateTime DateOfOrder { get; set; }
+        public int CartId { get; set; }
+    }
+}
